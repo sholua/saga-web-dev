cec3bd1fb719b12d37434a7ef71baff2cd02a4f5
Oleksandr Shevchuk <oleksandr.shevchuk@lanars.com>
2022-08-15 14:21:48 +0300

    Redux-saga integration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,23 @@
-import { createStore, compose } from "redux";
-import reducer from "./reducers";
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import reducer from './reducers';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancers =
-  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 
-const configureStore = (preloadedState) =>
-  createStore(reducer, preloadedState, composeEnhancers());
+const configureStore = preloadedState => createStore(
+  reducer,
+  preloadedState,
+  composeEnhancers(applyMiddleware(sagaMiddleware)),
+);
 
 const store = configureStore({});
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
