687cf17beda1dbddba9faa51ada5d8a7531f8958
Oleksandr Shevchuk <oleksandr.shevchuk@lanars.com>
2022-08-15 15:30:39 +0300

    Parallel requests

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
 import { useSelector, useDispatch } from "react-redux";
-import {
-  increaseCount,
-  decreaseCount,
-  getLatestNews,
-} from "./redux/actions/actionCreator";
+import { getNews } from "./redux/actions/actionCreator";
+import News from "./components/news/news";
+import "./index.css";
 
 const App = () => {
-  const count = useSelector((store) => store?.counter?.count || 0);
+  const latestNews = useSelector((store) => store?.news?.latestNews || []);
+  const popularNews = useSelector((store) => store?.news?.popularNews || []);
   const dispatch = useDispatch();
 
-  const handleIncrease = () => {
-    dispatch(increaseCount());
-  };
-
-  const handleDecrease = () => {
-    dispatch(decreaseCount());
-  };
-
   const handleNews = () => {
-    dispatch(getLatestNews());
+    dispatch(getNews());
   };
 
   return (
     <div>
-      <button onClick={handleIncrease}>+1</button>
-      <button onClick={handleDecrease}>-1</button>
       <button onClick={handleNews}>Get News</button>
-      <h1>{count}</h1>
+      <div className="container">
+        <div className="container-item">
+          <News news={latestNews} title="Latest News" />
+        </div>
+        <div className="container-item">
+          <News news={popularNews} title="Popular News" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/api/index.js
+++ src/api/index.js
@@ -4,3 +4,10 @@ export const getLatestNews = async (searchQuery) => {
   );
   return await res.json();
 };
+
+export const getPopularNews = async () => {
+  const res = await fetch(
+    `https://hn.algolia.com/api/v1/search?hitsPerPage=10&page=0`
+  );
+  return await res.json();
+};
