e926242016cff21926879cf60dd8194cfad3d46e
Oleksandr Shevchuk <oleksandr.shevchuk@lanars.com>
2022-08-15 14:49:47 +0300

    Save data to the store

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
-export const getLatestNews = async () => {
+export const getLatestNews = async (searchQuery) => {
   const res = await fetch(
-    "https://hn.algolia.com/api/v1/search?query=react&hitsPerPage=10&page=0"
+    `https://hn.algolia.com/api/v1/search?query=${searchQuery}&hitsPerPage=10&page=0`
   );
   return await res.json();
 };
---
+++ src/redux/reducers/news.js
@@ -0,0 +1,19 @@
+import { SET_LATEST_NEWS } from "../constants";
+
+const initialState = {
+  latestNews: [],
+};
+
+const news = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case SET_LATEST_NEWS:
+      return {
+        ...state,
+        latestNews: [...state.latestNews, ...payload],
+      };
+    default:
+      return state;
+  }
+};
+
+export default news;
